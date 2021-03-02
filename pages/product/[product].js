66353cc10342c564eff70ccb0ec5b9427d71f1a6
Jeff McElhannon <jeffpm@gmail.com>
2021-03-02 09:36:29 -0500

    fix dynamic routing

--- pages/product/[product].js
+++ pages/product/[product].js
@@ -6,18 +6,30 @@ import Layout from "~/components/Layout";
 import products from "~/resources/products.yml";
 
 
-export default function Product() {
-    const router = useRouter()
-    const { product } = router.query
+export default function Product({ product }) {
     return (
         <Layout>
         <main className="flex flex-col items-center flex-1 text-left py-2">
         
-            <p>{products[product].name}</p>
-            <Image src={`/images/${products[product].image}`} height="200" width="200"></Image>
-            <p>{products[product].description}</p>
+            <p>{product.name}</p>
+            <Image src={`/images/${product.image}`} height="200" width="200"></Image>
+            <p>{product.description}</p>
         </main>
         </Layout>
-        
     )
 }
+
+export async function getStaticPaths() {  
+    // Get the paths we want to pre-render based on posts
+    const paths = Object.keys(products).map((key, index) => ({
+        params: { product: key },
+      }))
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+    return { paths, fallback: false }
+  }
+
+export async function getStaticProps({ params }) {
+    const datProduct = products[params.product]
+    return { props: {product: datProduct} } 
+  }
